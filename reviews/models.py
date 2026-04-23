64120cf44afab95218a26ece9b13a64983c0a273
Liv <jcox0893@live.com>
2026-04-23 12:15:54 -0500

    created book review list

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.TextField(max_length=100)
+    author = models.TextField(max_length=100)
+    genre = models.TextField(max_length=100)
+    publication = models.DateField()
+
+    def __str__(self):
+        return self.title
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import BookListView
 
 app_name = 'reviews'
-urlpatterns = []
+urlpatterns = [
+    path('', BookListView.as_view(), name='list'),
+]
